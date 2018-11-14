cf52169b282fbfb56e91e0958be9c82ba9f62dc5
Andrew Dawson <dawso.andrew@gmail.com>
2018-11-14 01:26:06 -0500

    Adds the basic mechanics for a single game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,63 @@
 "use strict";
 
-var keyDisplay = document.getElementById("fella");
 var keysDown = [];
 
 
-function handleKeyDown(key) {
-  keyDisplay.textContent = key;
+function isAlphabetic(string) {
+  const value = string.toLowerCase();
+  return string.length === 1 && value >= "a" && value <= "z";
 }
 
+
+class Game {
+  constructor() {
+    this.currentWord = "";
+    this.currentWordReadout = document.getElementById("current-word");
+    this.lettersGuessedReadout = document.getElementById("letters-guessed");
+    this.lettersGuessed = [];
+  }
+
+  begin() {
+    this.currentWord = "secret";
+    this.lettersGuessed = [];
+    this.updateCurrentWordReadout();
+    this.updateLettersGuessedReadout();
+  }
+
+  guessLetter(letter) {
+    if (!this.lettersGuessed.includes(letter)) {
+      this.lettersGuessed.push(letter);
+      this.updateCurrentWordReadout();
+      this.updateLettersGuessedReadout();
+    }
+  }
+
+  updateCurrentWordReadout() {
+    let lettersShown = "";
+    for (const letter of this.currentWord) {
+      if (this.lettersGuessed.includes(letter)) {
+        lettersShown += letter;
+      } else {
+        lettersShown += "_";
+      }
+    }
+    this.currentWordReadout.textContent = lettersShown;
+  }
+
+  updateLettersGuessedReadout() {
+    this.lettersGuessedReadout.textContent = this.lettersGuessed.join(", ");
+  }
+}
+
+
+var game = new Game();
+game.begin();
 
 document.addEventListener("keydown", function(event) {
   const repeating = keysDown[event.key];
   keysDown[event.key] = true;
-  if (!repeating) {
-    handleKeyDown(event.key);
+  if (!repeating && isAlphabetic(event.key)) {
+    game.guessLetter(event.key);
   }
 });
 
