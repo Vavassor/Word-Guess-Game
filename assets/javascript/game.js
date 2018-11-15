0aed654506a83a8b8db6a8adc9cf0f896186380c
Andrew Dawson <dawso.andrew@gmail.com>
2018-11-14 23:00:03 -0500

    Adds a basic layout to the page and a placeholder fruit theme.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,49 @@
 
 var keysDown = [];
 
+
+function isAlphabetic(string) {
+  const value = string.toLowerCase();
+  return string.length === 1 && value >= "a" && value <= "z";
+}
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+  }
+  return a;
+}
+
+function WordPick(word, image) {
+  this.word = word;
+  this.image = image;
+}
+
+
 var game = {
   currentWord: "",
   currentWordReadout: document.getElementById("current-word"),
   guessesRemaining : 0,
   guessesRemainingReadout: document.getElementById("guesses-remaining"),
+  lastWord: "",
+  lastWordReadout: document.getElementById("last-word"),
   lettersGuessed: [],
   lettersGuessedReadout: document.getElementById("letters-guessed"),
   losses: 0,
   lossesReadout: document.getElementById("losses"),
+  winImage: "",
+  winImageDisplay: document.getElementById("win-image"),
   wins: 0,
   winsReadout: document.getElementById("wins"),
   wordIndex: 0,
-  words: shuffle(["secret", "wow", "mangoes"]),
+  words: shuffle([
+    new WordPick("kiwi", "assets/images/kiwi.png"),
+    new WordPick("apple", "assets/images/apple.jpg"),
+    new WordPick("mango", "assets/images/mango.jpg"),
+  ]),
 
   determineLettersShown: function() {
     let lettersShown = "";
@@ -54,16 +84,19 @@ var game = {
 
   lose: function() {
     this.losses += 1;
+    this.winImageDisplay.src = "";
     this.startNewGame();
   },
 
   pickWord: function() {
-    const word = this.words[this.wordIndex];
+    const pick = this.words[this.wordIndex];
     this.wordIndex = (this.wordIndex + 1) % this.words.length;
-    return word;
+    this.winImage = pick.image;
+    return pick.word;
   },
 
   startNewGame: function() {
+    this.lastWord = this.currentWord;
     this.currentWord = this.pickWord();
     this.lettersGuessed = [];
     this.guessesRemaining = 10;
@@ -72,34 +105,26 @@ var game = {
   },
 
   updateReadouts: function(lettersShown) {
+    const lettersGuessed = this.lettersGuessed.join(", ");
+    if (lettersGuessed.length == 0) {
+      this.lettersGuessedReadout.textContent = "·";
+    } else {
+      this.lettersGuessedReadout.textContent = lettersGuessed;
+    }
+
     this.currentWordReadout.textContent = lettersShown;
-    this.lettersGuessedReadout.textContent = this.lettersGuessed.join(", ");
     this.guessesRemainingReadout.textContent = this.guessesRemaining;
     this.winsReadout.textContent = this.wins;
     this.lossesReadout.textContent = this.losses;
+    this.lastWordReadout.textContent = this.lastWord;
   },
 
   win: function() {
     this.wins += 1;
+    this.winImageDisplay.src = this.winImage;
     this.startNewGame();
   },
 };
-
-
-function isAlphabetic(string) {
-  const value = string.toLowerCase();
-  return string.length === 1 && value >= "a" && value <= "z";
-}
-
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
-  }
-  return a;
-}
 
 
 game.startNewGame();
