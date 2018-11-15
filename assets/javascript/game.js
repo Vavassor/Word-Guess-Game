0411dcf87a95bd807938daae3b6a91abc0d04014
Andrew Dawson <dawso.andrew@gmail.com>
2018-11-14 20:44:01 -0500

    Adds remaining guesses, wins, and losses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,37 +2,26 @@
 
 var keysDown = [];
 
+var game = {
+    currentWord: "",
+    currentWordReadout: document.getElementById("current-word"),
+    guessesRemainingReadout: document.getElementById("guesses-remaining"),
+    guessMax: 0,
+    lettersGuessed: [],
+    lettersGuessedReadout: document.getElementById("letters-guessed"),
+    losses: 0,
+    lossesReadout: document.getElementById("losses"),
+    wins: 0,
+    winsReadout: document.getElementById("wins"),
 
-function isAlphabetic(string) {
-  const value = string.toLowerCase();
-  return string.length === 1 && value >= "a" && value <= "z";
-}
-
-
-class Game {
-  constructor() {
-    this.currentWord = "";
-    this.currentWordReadout = document.getElementById("current-word");
-    this.lettersGuessedReadout = document.getElementById("letters-guessed");
-    this.lettersGuessed = [];
-  }
-
-  begin() {
+  startNewGame: function() {
     this.currentWord = "secret";
     this.lettersGuessed = [];
-    this.updateCurrentWordReadout();
-    this.updateLettersGuessedReadout();
-  }
+    this.guessMax = 10;
+    this.updateReadouts();
+  },
 
-  guessLetter(letter) {
-    if (!this.lettersGuessed.includes(letter)) {
-      this.lettersGuessed.push(letter);
-      this.updateCurrentWordReadout();
-      this.updateLettersGuessedReadout();
-    }
-  }
-
-  updateCurrentWordReadout() {
+  determineLettersShown: function() {
     let lettersShown = "";
     for (const letter of this.currentWord) {
       if (this.lettersGuessed.includes(letter)) {
@@ -41,17 +30,58 @@ class Game {
         lettersShown += "_";
       }
     }
-    this.currentWordReadout.textContent = lettersShown;
-  }
+    return lettersShown;
+  },
 
-  updateLettersGuessedReadout() {
+  guessLetter: function(letter) {
+    if (this.lettersGuessed.includes(letter)) {
+      return;
+    }
+
+    this.lettersGuessed.push(letter);
+
+    const guessesRemaining = this.guessMax - this.lettersGuessed.length;
+    if (guessesRemaining <= 0) {
+      this.lose();
+      return;
+    }
+
+    let lettersShown = this.determineLettersShown();
+    if (lettersShown === this.currentWord) {
+      this.win();
+      return;
+    }
+
+    this.updateReadouts(lettersShown);
+  },
+
+  lose: function() {
+    this.losses += 1;
+    this.startNewGame();
+  },
+
+  updateReadouts: function(lettersShown) {
+    this.currentWordReadout.textContent = lettersShown;
     this.lettersGuessedReadout.textContent = this.lettersGuessed.join(", ");
-  }
+    this.guessesRemainingReadout.textContent = this.guessMax - this.lettersGuessed.length;
+    this.winsReadout.textContent = this.wins;
+    this.lossesReadout.textContent = this.losses;
+  },
+
+  win: function() {
+    this.wins += 1;
+    this.startNewGame();
+  },
+};
+
+
+function isAlphabetic(string) {
+  const value = string.toLowerCase();
+  return string.length === 1 && value >= "a" && value <= "z";
 }
 
 
-var game = new Game();
-game.begin();
+game.startNewGame();
 
 document.addEventListener("keydown", function(event) {
   const repeating = keysDown[event.key];
